aee4875fc0083b5d154d039841463bb42ad81489
Asher Pope <asherpope@gmail.com>
2023-01-13 14:54:36 -0500

    Initial Commit

---
+++ New Members Check In/New_Members_Check_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Members_Check_InApp.swift
+//  New Members Check In
+//
+//  Created by Asher Pope on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct New_Members_Check_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
