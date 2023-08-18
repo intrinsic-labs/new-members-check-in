9d3c6752ab52816b4ceba4431a8c76c8e80e9abc
Asher Pope <asherpope@gmail.com>
2023-08-18 14:20:37 -0400

    Add support for decoding over 100 records of JSON from Airtable

--- New Members Check In/data/remote/Airtable.swift
+++ New Members Check In/data/remote/Airtable.swift
@@ -17,9 +17,9 @@ class AirtableUser: ObservableObject {
     @Published var isCurrentlyViewing: CurrentView = .loginView
     @Published var isAuthenticated = false
     
-//    FOR TESTING:
-//    API Token: patd2M1JxX7MmxCY0.5b0678f6cbe5a823c9b41f3e4666ced64ebf4ffbd91efb96613f498d8587e57a
-//    Base ID: appOF6u4kcIz7OPNR
+    //    FOR TESTING:
+    //    API Token: patd2M1JxX7MmxCY0.5b0678f6cbe5a823c9b41f3e4666ced64ebf4ffbd91efb96613f498d8587e57a
+    //    Base ID: appOF6u4kcIz7OPNR
     @Published var apiToken: String = ""
     let baseID: String = "appM9E6gadZ36GN2Y"
     
@@ -39,8 +39,19 @@ class Airtable: ObservableObject {
     }
     
     func loadMembers(user: AirtableUser) async {
-        let url = URL(string: "https://api.airtable.com/v0/\(user.baseID)/New%20Members?view=Grid%20View")!
-        var request = URLRequest(url: url)
+        
+        // build out url to Airtable base
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.airtable.com"
+        components.path = "/v0/\(user.baseID)/New Members"
+        components.queryItems = [
+            URLQueryItem(name: "offset", value: "0"),
+            URLQueryItem(name: "view", value: "Grid View")
+        ]
+        print(components.string ?? "error with url")
+        
+        var request = URLRequest(url: components.url!)
         request.setValue("Bearer \(user.apiToken)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "GET"
         
@@ -49,7 +60,31 @@ class Airtable: ObservableObject {
             if let decoded = try? JSONDecoder().decode(Records.self, from: data) {
                 
                 self.listOfAllMembers = decoded.records
-                print("Success!")
+                print("Offset received: " + decoded.offest)
+                
+                if decoded.offest != "" { // if there are more records, get those too
+                    print("Trying another request...")
+                    components.queryItems = [
+                        URLQueryItem(name: "offset", value: decoded.offest)
+                    ]
+                    request = URLRequest(url: components.url!)
+                    request.setValue("Bearer \(user.apiToken)", forHTTPHeaderField: "Authorization")
+                    request.httpMethod = "GET"
+                    
+                    
+                    do {
+                        let (data, _) = try await URLSession.shared.data(for: request)
+                        if let decoded = try? JSONDecoder().decode(Records.self, from: data) {
+                            
+                            self.listOfAllMembers += decoded.records
+                            //print(String(data: data, encoding: .utf8) ?? "")
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                    
+                }
+                
             } else {
                 print(String(data: data, encoding: .utf8) ?? "")
                 print("Airtable.loadMembers: Undecodable data received.")
@@ -61,67 +96,63 @@ class Airtable: ObservableObject {
     }
     
     func loadDates(user: AirtableUser) async {
-        let url = URL(string: "https://api.airtable.com/v0/\(user.baseID)/Attendance?view=Grid%20View")!
+        let urlStr = "https://api.airtable.com/v0/\(user.baseID)/Attendance?view=Grid%20View"
+        let url = URL(string: urlStr)!
         var request = URLRequest(url: url)
         request.setValue("Bearer \(user.apiToken)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "GET"
         
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                Task {
-                    await MainActor.run {
-                        if let decoded = try? JSONDecoder().decode(Records.self, from: data) {
-                            // Update dates array
-                            self.listOfAllDates = decoded.records
-                        } else {
-                            print(String(data: data, encoding: .utf8) ?? "")
-                            print("Could not decode data.")
-                        }
-                    }
-                }
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            if let decoded = try? JSONDecoder().decode(Records.self, from: data) {
+                
+                // Update dates array
+                self.listOfAllDates = decoded.records
+                //print(String(data: data, encoding: .utf8) ?? "")
+                
             } else {
-                print("Unidentifiable error")
+                print(String(data: data, encoding: .utf8) ?? "")
+                print("Could not decode data.")
             }
+        } catch {
+            print(error.localizedDescription)
         }
-        task.resume()
     }
     
-        
+    
     func updateRecords(for selection: [Record], user: AirtableUser) async {
-            if selection != [defaultRecord] {
-                let url = URL(string: "https://api.airtable.com/v0/\(user.baseID)/New%20Members")!
-                var request = URLRequest(url: url)
-                request.setValue("Bearer \(user.apiToken)", forHTTPHeaderField: "Authorization")
-                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                request.httpMethod = "PATCH"
-                
-                // Convert the current selection array into sendable objects
-                var nameSelectionSendableRecords = [SendableRecord]()
-                for name in selection {
-                    let sendableFields = SendableFields(attendance: name.fields.attendance)
-                    let sendableRecord = SendableRecord(id: name.id, fields: sendableFields)
-                    nameSelectionSendableRecords.append(sendableRecord)
-                }
-                let toEncode = SendableRecords(records: nameSelectionSendableRecords)
-                print(toEncode.records.description)
-                guard let encoded = try? JSONEncoder().encode(toEncode) else {
-                    print("Airtable.updateRecords: could not encode let constant 'toEncode'")
-                    return
-                }
-                // Make the HTTP request
-                do {
-                    let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
-                    print(String(data: data, encoding: .utf8)!)
-                   
-                } catch {
-                    print(error.localizedDescription)
-                }
-            } else {
-                print("Airtable.updateRecords: provided selection is [defaultRecord].")
+        if selection != [defaultRecord] {
+            let url = URL(string: "https://api.airtable.com/v0/\(user.baseID)/New%20Members")!
+            var request = URLRequest(url: url)
+            request.setValue("Bearer \(user.apiToken)", forHTTPHeaderField: "Authorization")
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "PATCH"
+            
+            // Convert the current selection array into sendable objects
+            var nameSelectionSendableRecords = [SendableRecord]()
+            for name in selection {
+                let sendableFields = SendableFields(attendance: name.fields.attendance)
+                let sendableRecord = SendableRecord(id: name.id, fields: sendableFields)
+                nameSelectionSendableRecords.append(sendableRecord)
             }
+            let toEncode = SendableRecords(records: nameSelectionSendableRecords)
+            print(toEncode.records.description)
+            guard let encoded = try? JSONEncoder().encode(toEncode) else {
+                print("Airtable.updateRecords: could not encode let constant 'toEncode'")
+                return
+            }
+            // Make the HTTP request
+            do {
+                let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+                print(String(data: data, encoding: .utf8)!)
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        } else {
+            print("Airtable.updateRecords: provided selection is [defaultRecord].")
         }
+    }
     
     func authenticateUser(_ user: AirtableUser) async {
         let url = URL(string: "https://api.airtable.com/v0/\(user.baseID)/New%20Members?maxRecords=1")!
@@ -157,4 +188,5 @@ class Airtable: ObservableObject {
         
     }
 }
+
 
